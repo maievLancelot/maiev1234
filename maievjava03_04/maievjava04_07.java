4c8b41e0d78616b24be9244596787e6ea7eb8732
maiev <1920220186@qq.com>
2021-01-28 13:44:33 +0800

    3

--- maievjava03_04/maievjava04_07.java
+++ maievjava03_04/maievjava04_07.java
@@ -14,3 +14,69 @@ public class maievjava04_07 {
 		
 	}
 }
+package maievjava04_07;
+
+import java.io.InputStream;
+
+import javafx.scene.image.Image;
+
+public class maievjava04_07 {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=10;i++) {
+			
+			System.out.println("maiev");
+		}
+		
+	}
+}
+
+package maievjava04_07;
+
+import java.io.InputStream;
+
+import javafx.scene.image.Image;
+
+public class maievjava04_07 {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=10;i++) {
+			
+			System.out.println("maiev");
+		}
+		
+	}
+}
+
+package maievjava04_07;
+
+import java.io.InputStream;
+
+import javafx.scene.image.Image;
+
+public class maievjava04_07 {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=10;i++) {
+			
+			System.out.println("maiev");
+		}
+		
+	}
+}
+package maievjava04_07;
+
+import java.io.InputStream;
+
+import javafx.scene.image.Image;
+
+public class maievjava04_07 {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=10;i++) {
+			
+			System.out.println("maiev");
+		}
+		
+	}
+}
