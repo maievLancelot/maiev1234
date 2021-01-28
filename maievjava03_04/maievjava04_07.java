d5cd7eb06265453c80f60f46c89428bf2b657426
maiev <1920220186@qq.com>
2021-01-28 13:43:50 +0800

    2

--- maievjava03_04/maievjava04_07.java
+++ maievjava03_04/maievjava04_07.java
@@ -1,16 +1,5 @@
-package maievjava04_07;
-
-import java.io.InputStream;
-
-import javafx.scene.image.Image;
-
-public class maievjava04_07 {
-	public static void main(String[] args) {
-		
-		for(int i=0;i<=10;i++) {
-			
-			System.out.println("maiev");
-		}
-		
-	}
-}
+maiev
+maiev
+maiev
+maiev
+maievmaievvmaiev
